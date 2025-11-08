b9251e62b887241237725ac989f56e36a1ea05dd
Babajay <babajayfx@gmail.com>
2025-11-08 17:01:45 +0100

    Place basic_operations.py under python_introduction and correct variables/output

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,9 +1,9 @@
 #performing basic arithmetic operations addition, subtraction and multiplication
-number_1 = 10
-number_2 = 5
-result = number_1 + number_2
-difference = number_1 - number_2
-product = number_1 * number_2
+number1 = 10
+number2 = 5
+result = number1 + number2
+difference = number1 - number2
+product = number1 * number2
 print("Addition of 10 and 5 is", result)
 print("Subtraction of 10 and 5 is", difference)
-print("multiplication of 10 and 5 is", product)
+print("Multiplication of 10 and 5 is", product)
