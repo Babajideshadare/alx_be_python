5e3946be8669eed95d6a878bae418969898de589
Babajay <babajayfx@gmail.com>
2025-11-08 16:27:57 +0100

    add basic_operations.py to python_introduction

---
+++ basic_operations.py
@@ -0,0 +1,9 @@
+#performing basic arithmetic operations addition, subtraction and multiplication
+number_1 = 10
+number_2 = 5
+result = number_1 + number_2
+difference = number_1 - number_2
+product = number_1 * number_2
+print("Addition of 10 and 5 is", result)
+print("Subtraction of 10 and 5 is", difference)
+print("multiplication of 10 and 5 is", product)
