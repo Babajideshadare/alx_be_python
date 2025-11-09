3d2ee69e2e45061dbb1818b3e1e0abd567976fbc
Babajay <babajayfx@gmail.com>
2025-11-09 13:48:25 +0100

    add simple_interest.py to python_introduction

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,6 @@
+# solving simple interest calculation using I = P*R*T
+principal = 1000 # Principal amount in dollars
+rate = 0.05 # Annual interest rate in decimal (5%)
+time = 3 # time representing 3 years 
+interest = principal * rate * time 
+print("The simple interest is:", interest)
