ff851cfa5f87ca16363259069337ff385613492e
Babajay <babajayfx@gmail.com>
2025-11-16 23:31:22 +0100

    changed the variales in multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,8 @@
-X = int(input("Enter a number to see its multiplication table: "))
-for Y in range(1, 11):
-    Z = X * Y
-    print(f"{X} x {Y} = {Z}")
+# multiplication_table.py
+
+number = int(input("Enter a number to see its multiplication table: "))
+
+for i in range(1, 11):
+    result = number * i
+    print(f"{number} * {i} = {result}")
 
